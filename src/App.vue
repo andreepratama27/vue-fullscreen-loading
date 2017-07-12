7369511d6572e42d5a6807f9695ecec756880699
andreepratama27 <andreepratama27@gmail.com>
2017-07-12 13:31:12 +0700

    add component loading

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
 <template>
     <div>
+        <testing></testing>
     </div>
 </template>
+
+<script>
+    import Testing from './testing.vue'
+
+    export default {
+        component: {
+            Testing
+        }
+    }
+</script>
